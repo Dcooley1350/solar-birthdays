93619333e8e578350f55d580142ff6141fd6e033
Devin Cooley <dcooley1350@gmail.com>
2019-09-06 18:27:40 -0700

    fix typos, add css and finish project

--- src/main.js
+++ src/main.js
@@ -7,24 +7,8 @@ import { User } from './project.js';
 import { GalacticAges } from './project.js';
 import { GalacticYearsLeft2Live } from './project.js';
 
-// function displayResults() {
-//   $("#earth-age").text(newUser.ageEarth);
-//   $("#earth-yl").text(newUser.earthYearsLeft2Live);
-//   $("#mercury-age").text(newGalacticAge.agemercury);
-//   $("#mercury-yl").text(newGalacticYearsLeft2Live.mercuryYearsLeft2Live);
-//   $("#venus-age").text(newGalacticAge.ageVenus);
-//   $("#venus-yl").text(newGalacticYearsLeft2Live.venusYearsLeft2Live);
-//   $("#mars-age").text(newGalacticAge.ageMars);
-//   $("#mars-yl").text(newGalacticYearsLeft2Live.marsYearsLeft2Live);
-//   $("#jupiter-age").text(newGalacticAge.ageJupiter);
-//   $("#jupiter-yl").text(newGalacticYearsLeft2Live.jupiterYearsLeft2Live);
-//   $("#input-panel").hide();
-//   $("#output-panel").show();
-// }
-
 $(document).ready(function(){
   $("#submit-info").submit(function(event){
-    console.log("tacos");
     event.preventDefault();
     $("#idiot").hide();
     let inputtedAge = $("#age").val();
@@ -32,7 +16,7 @@ $(document).ready(function(){
     let newUser = new User(inputtedAge, inputtedLE);
     let newGalacticAge = new GalacticAges(inputtedAge);
     let newGalacticYearsLeft2Live = new GalacticYearsLeft2Live();
-    const ageReal = newUser.realAge()
+    const ageReal = newUser.realAge();
     if (ageReal == true) {
       newUser.earthYearsLeft();
       newGalacticYearsLeft2Live.earthYearsLeft2Live = newUser.earthYearsLeft2Live;
@@ -44,10 +28,6 @@ $(document).ready(function(){
       newGalacticYearsLeft2Live.venusYearsLeft();
       newGalacticYearsLeft2Live.marsYearsLeft();
       newGalacticYearsLeft2Live.jupiterYearsLeft();
-      console.log(newGalacticYearsLeft2Live.earthYearsLeft2Live);
-      console.log(newGalacticYearsLeft2Live.jupiterYearsLeft());
-      console.log(newGalacticYearsLeft2Live.jupiterYearsLeft2Live)
-
       $("#earth-age").text(newUser.ageEarth);
       $("#earth-yl").text(newUser.earthYearsLeft2Live);
       $("#mercury-age").text(newGalacticAge.ageMercury);
@@ -58,8 +38,8 @@ $(document).ready(function(){
       $("#mars-yl").text(newGalacticYearsLeft2Live.marsYearsLeft2Live);
       $("#jupiter-age").text(newGalacticAge.ageJupiter);
       $("#jupiter-yl").text(newGalacticYearsLeft2Live.jupiterYearsLeft2Live);
-      $("#input-panel").hide();
-      $("#output-panel").show();
+      $(".input-panel").hide();
+      $(".output-panel").show();
     } else {
       $("#idiot").show();
       newUser.ageEarth = [];
