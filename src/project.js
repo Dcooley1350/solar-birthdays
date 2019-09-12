30a8d7079e203cf5f8358388e2a26eccb48e0984
Devin Cooley <dcooley1350@gmail.com>
2019-09-11 22:56:10 -0700

    Begin refactor by deleting all returns in prototypes and all unused argurments in constructors

--- src/project.js
+++ src/project.js
@@ -1,8 +1,8 @@
 export class User {
-  constructor(ageEarth, lifeExpectancy, earthYearsLeft2Live) {
-    this.ageEarth = ageEarth;
-    this.lifeExpectancy = lifeExpectancy;
-    this.earthYearsLeft2Live = earthYearsLeft2Live;
+  constructor() {
+    this.ageEarth;
+    this.lifeExpectancy;
+    this.earthYearsLeft2Live;
   }
   realAge() {
     if(120>this.ageEarth && this.ageEarth> 0){
@@ -19,53 +19,45 @@ export class User {
 
 
 export class GalacticAges {
-  constructor(ageEarth,ageMercury, ageVenus, ageMars, ageJupiter) {
-    this.ageEarth = ageEarth;
-    this.ageMercury = ageMercury;
-    this.ageVenus = ageVenus;
-    this.ageMars = ageMars;
-    this.ageJupiter= ageJupiter;
+  constructor() {
+    this.ageEarth;
+    this.ageMercury;
+    this.ageVenus;
+    this.ageMars;
+    this.ageJupiter;
   }
   mercuryAge() {
     this.ageMercury = Math.round(this.ageEarth/0.24*100)/100;
-    return this.ageMercury;
   }
   venusAge() {
     this.ageVenus = Math.round(this.ageEarth/0.62*100)/100;
-    return this.ageVenus;
   }
   marsAge() {
     this.ageMars = Math.round(this.ageEarth/1.88*100)/100;
-    return this.ageMars;
   }
   jupiterAge() {
     this.ageJupiter = Math.round(this.ageEarth/11.86*100)/100;
-    return this.ageJupiter;
   }
 }
 
 export class GalacticYearsLeft2Live {
-  constructor( earthYearsLeft2Live, mercuryYearsLeft2Live, venusYearsLeft2Live, marsYearsLeft2Live, jupiterYearsLeft2Live){
-    this.earthYearsLeft2Live = earthYearsLeft2Live;
-    this.mercuryYearsLeft2Live = mercuryYearsLeft2Live;
-    this.venusYearsLeft2Live = venusYearsLeft2Live;
-    this.marsYearsLeft2Live = marsYearsLeft2Live;
-    this.jupiterYearsLeft2Live = jupiterYearsLeft2Live;
+  constructor(){
+    this.earthYearsLeft2Live;
+    this.mercuryYearsLeft2Live;
+    this.venusYearsLeft2Live;
+    this.marsYearsLeft2Live;
+    this.jupiterYearsLeft2Live;
   }
   mercuryYearsLeft(){
     this.mercuryYearsLeft2Live = Math.round((this.earthYearsLeft2Live/0.24*100))/100;
-    return this.mercuryYearsLeft2Live;
   }
   venusYearsLeft(){
     this.venusYearsLeft2Live = Math.round(this.earthYearsLeft2Live/0.62*100)/100;
-    return this.venusYearsLeft2Live;
   }
   marsYearsLeft(){
     this.marsYearsLeft2Live = Math.round(this.earthYearsLeft2Live/1.88*100)/100;
-    return this.marsYearsLeft2Live;
   }
   jupiterYearsLeft(){
     this.jupiterYearsLeft2Live = Math.round(this.earthYearsLeft2Live/11.86*100)/100;
-    return this.jupiterYearsLeft2Live;
   }
 }
